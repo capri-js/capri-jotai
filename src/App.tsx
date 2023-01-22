feec1e0b231c449ed69043d2bcb1618f4283cc13
Felix Gnass <fgnass@gmail.com>
2023-01-22 22:26:58 +0100

    Add GitHub link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import CounterIsland from "./Counter.island.jsx";
 import "./App.css";
+import { Footer } from "./Footer.jsx";
 
 export function App() {
   return (
@@ -8,12 +9,13 @@ export function App() {
         Share state across Capri islands with{" "}
         <a href="https://jotai.org">Jotai</a> 👻
       </h1>
-      <section>
+      <p>
         Here is an island with a counter: <CounterIsland />
-      </section>
-      <section>
+      </p>
+      <p>
         And here a second island, that shares the same state: <CounterIsland />
-      </section>
+      </p>
+      <Footer />
     </main>
   );
 }
